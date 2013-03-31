5ef16f5f7e789710ce33e1d8c822c9d7a3f329ea
Alexander Ramirez <adr2370@gmail.com>
2013-03-31 01:20:14 -0700

    More tried

--- schedule.php
+++ schedule.php
@@ -116,7 +116,7 @@
 			for($i=0;$i<count($allClasses);$i++) {
 				$exploded=explode(' ',$allClasses[$i]);
 				$which="";
-				if(count($exploded)<2) continue;
+				if(count($exploded)<4) continue;
 				for($j=0;$j<count($exploded)-1;$j++) $which=$which.$exploded[$j]." ";
 				if($which!=$last) {
 					echo "<br>"$which."<br>";
