b93d480b2168a32ecb85d917fc81b3634f3ff5bc
Alexander Ramirez <adr2370@gmail.com>
2013-04-14 17:04:31 -0700

    3 bottom limit

--- make.php
+++ make.php
@@ -11,7 +11,7 @@ $result=mysql_query($query);
 $days=array("Monday","Tuesday","Wednesday","Thursday","Friday");
 $times=array("10am-12pm","11am-1pm","12pm-2pm","1pm-3pm","2pm-4pm","3pm-5pm","4pm-6pm");
 $actualTime=array("10am-11am","11am-12pm","12pm-1pm","1pm-2pm","2pm-3pm","3pm-4pm","4pm-5pm","5pm-6pm");
-$bottomLimit=2;
+$bottomLimit=3;
 if(mysql_num_rows($result)>0)
 {
 	while($res = mysql_fetch_assoc($result))
